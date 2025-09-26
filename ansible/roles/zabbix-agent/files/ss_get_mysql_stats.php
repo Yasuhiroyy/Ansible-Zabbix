ae5cdb9238d207377d453c227b45c08a852f8ff8
吉澤靖裕 <yoshizawayasuhiro@yoshizawayasuhironoMacBook-Pro.local>
2025-09-26 23:18:59 +0900

    Add ansible files except ansible_wordpress from menta

---
+++ ansible/roles/zabbix-agent/files/ss_get_mysql_stats.php
@@ -0,0 +1,2 @@
+curl -o roles/zabbix-agent/files/ss_get_mysql_stats.php \
+"https://git.zabbix.com/projects/ZBX/repos/zabbix/raw/templates/db/mysql_agent/ss_get_mysql_stats.php?at=refs/heads/release/5.0"
